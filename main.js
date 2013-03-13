48c725c550a665b9e5ac9ccb8f71f2100ba7a3ad
unobliged <brian.ortega@gmail.com>
2013-03-13 13:27:27 -0400

    2nd commit, complete redesign

--- main.js
+++ main.js
@@ -12,11 +12,22 @@ $(document).ready(function() {
 		$('#projects').removeClass('hidden');		
 	});
 	
+	/*
 	$('#projects img').hover(function(e){
 		var lg_src = $(this).attr('src').replace('tn_','');
 		$('#expand').toggleClass('hidden');
 		$('#expand img').attr('src', lg_src);
-	
-	
 	});
+	
+	
+	$(window).resize(function(){
+		var doc_height = $(document).height();
+		var win_height = $(window).height();
+		
+		if(doc_height > win_height){				
+			$('#projects').css('height', doc_height);
+		}
+	});
+	*/
+	
 });
